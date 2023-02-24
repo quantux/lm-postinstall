181783e1a93fc33f9e7934f809ad2ee25e521295
Matheus Faria <mths.faria@outlook.com>
2023-02-24 10:41:48 -0300

    Fix some issues with rsync copying whole folder instead of its contents

--- backup.sh
+++ backup.sh
@@ -7,8 +7,8 @@ if [[ $EUID -eq 0 ]]; then
 fi
 
 username=$(whoami)
-rsync -avr --info=PROGRESS --files-from=assets/backups/files-to-backup --exclude=".local/share/Trash" /home/mths/ ./assets/backups/home
-tar -cvzf ./assets/backups/home.tar.gz ./assets/backups/home
-rm -rf ./assets/backups/home
-gpg -c --cipher-algo AES256 ./assets/backups/home.tar.gz
-rm -rf ./assets/backups/home.tar.gz
+rsync -avr --info=PROGRESS --files-from=assets/backups/files-to-backup --exclude=".local/share/Trash" /home/$username/ assets/backups/home
+tar -cvzf assets/backups/home.tar.gz -C assets/backups home/
+rm -rf assets/backups/home
+gpg -c --cipher-algo AES256 assets/backups/home.tar.gz
+rm -rf assets/backups/home.tar.gz
