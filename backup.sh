2d0f6a9554b105f123d2fcf28cd1c39d7b690e9a
Matheus Faria <mths.faria@outlook.com>
2023-09-27 12:50:29 -0300

    use copy

--- backup.sh
+++ backup.sh
@@ -33,6 +33,6 @@ chown $RUID:$RUID assets/backups/home.tar.gz.gpg
 rm -rf assets/backups/home.tar.gz
 
 # Upload to google drive
-user_do "rclone sync -P ./assets/backups/home.tar.gz.gpg /home/$RUID/GDrive/Backups"
+user_do "rclone copy -P ./assets/backups/home.tar.gz.gpg /home/$RUID/GDrive/Backups"
 
 rm -rf assets/backups/home.tar.gz.gpg
