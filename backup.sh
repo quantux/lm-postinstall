ff205ffb2bb3fd1d38282899976eb39d694fa0c2
Matheus Faria <mths.faria@outlook.com>
2023-09-27 20:28:52 -0300

    systemd ocamlfuse service - remove

--- backup.sh
+++ backup.sh
@@ -11,6 +11,16 @@ user_do() {
     sudo -u ${RUID} /bin/bash -c "$1"
 }
 
+# start_uploading() {
+#   echo "Uploading file..."
+#   user_do "mkdir -p /home/$RUID/GDrive"
+#   user_do "google-drive-ocamlfuse /home/$RUID/GDrive"
+#   user_do "rsync -auv --progress --partial --delete assets/backups/home.tar.gz.gpg /home/$RUID/GDrive/Backups/"
+#   umount /home/$RUID/GDrive
+#   rm -rf /home/$RUID/GDrive
+#   # rm -rf assets/backups/home.tar.gz.gpg
+# }
+
 # asks for password confirmation
 while true; do
   read -s -p "GPG Password: " password
@@ -28,11 +38,20 @@ tar -cvzf assets/backups/home.tar.gz -C assets/backups home/
 rm -rf assets/backups/home
 
 # Encrypt using gpg
+echo "Encrypting..."
 echo $password2 | gpg --batch --yes --passphrase-fd 0 --pinentry-mode loopback -c --cipher-algo AES256 assets/backups/home.tar.gz
 chown $RUID:$RUID assets/backups/home.tar.gz.gpg
 rm -rf assets/backups/home.tar.gz
 
-# Upload to google drive
-rsync -auv --progress --partial --delete assets/backups/home.tar.gz.gpg /home/$RUID/GDrive/Backups/
-
-rm -rf assets/backups/home.tar.gz.gpg
+# Start uploading?
+# while true; do
+#   read -p "Start uploading? (Y/n): " response
+#   response=$(echo "$response" | tr '[:upper:]' '[:lower:]')
+#   if [ "$response" = "y" ] || [ "$response" = "yes" ]; then
+#     start_uploading
+#     break
+#   elif [ "$response" = "n" ] || [ "$response" = "no" ]; then
+#     echo "Backup completed."
+#     break
+#   fi
+# done
