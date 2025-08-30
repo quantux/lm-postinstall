a8722e98732cac37319d7118ec6a30adf24fe5e6
Matheus Faria <mths.faria@outlook.com>
2025-08-30 15:10:09 -0300

    Update.

--- recover.sh
+++ recover.sh
@@ -194,7 +194,7 @@ echo \
   $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 usermod -aG docker "${SUDO_USER:-$USER}"
 
 # Start containers
