441806d65715ca14b7632941fefe50f24a9819e4
Matheus Faria <mths.faria@outlook.com>
2025-10-09 22:04:28 -0300

    Update.

--- remember_backup.sh
+++ remember_backup.sh
@@ -3,12 +3,12 @@
 # Diretório do script e arquivo de controle .lock
 script_dir=$(dirname "$(realpath "$0")")
 backup_control_file="$script_dir/backup_control_file"
-current_week=$(date +%U)  # Semana atual do ano
-current_day=$(date +%u)   # Dia da semana (1 = Segunda, ..., 7 = Domingo)
+current_month=$(date +%m)  # Mês atual (01-12)
+current_day=$(date +%u)    # Dia da semana (1 = Segunda, ..., 7 = Domingo)
 
-# Verifica se o backup já foi feito na semana
+# Verifica se o backup já foi feito no mês
 backup_feito() {
-    [[ -f "$backup_control_file" && "$(cat "$backup_control_file")" == "$current_week" ]]
+    [[ -f "$backup_control_file" && "$(cat "$backup_control_file")" == "$current_month" ]]
 }
 
 # Pergunta ao usuário e executa o backup se necessário
@@ -18,7 +18,7 @@ executar_backup() {
 
     (
         cd "$script_dir" || exit 1
-        sudo ./backup.sh && echo "$current_week" > "$backup_control_file" && echo "Backup concluído!"
+        sudo ./backup.sh && echo "$current_month" > "$backup_control_file" && echo "Backup concluído!"
         echo "Processo concluído. Pressione Enter para sair."
         read -r
     )
@@ -26,11 +26,8 @@ executar_backup() {
 
 # Lógica principal
 if backup_feito; then
-    echo "O backup já foi feito esta semana."
-elif [[ "$current_day" -eq 7 ]]; then
-    echo "Hoje é domingo!"
-    executar_backup
+    echo "O backup já foi feito este mês."
 else
-    echo "O backup ainda não foi feito esta semana."
+    echo "O backup ainda não foi feito este mês."
     executar_backup
 fi
