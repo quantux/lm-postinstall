06ad0186ac5aabdc04c0c49c52540b5c69461f2a
Matheus Faria <mths.faria@outlook.com>
2026-08-05 15:45:48 -0300

    Update.

--- steps/03_apt_packages.sh
+++ steps/03_apt_packages.sh
@@ -4,5 +4,7 @@
 
 step_03_apt_packages() {
     show_message "Instalando pacotes apt (pacotes_apt.txt)"
+    # Aceita rodar o iperf3 como daemon sem confirmar durante a instalação
+    echo "iperf3 iperf3/start_daemon boolean true" | debconf-set-selections
     apt-get install -y $(cat pacotes_apt.txt)
 }
